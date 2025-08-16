0682fa89c31df9bd168d18250cdeb7613b0a62f8
_eipai <3321089348@qq.com>
2025-08-17 00:24:31 +0800

    chore: 为paint方法添加了注释

--- src/model/room_delegate.cpp
+++ src/model/room_delegate.cpp
@@ -12,9 +12,13 @@ RoomDelegate::RoomDelegate(QObject *parent) : QStyledItemDelegate{parent} {}
 
 void RoomDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
                          const QModelIndex &index) const {
+    // 暂存painter状态，再函数最后使用restore恢复，可以确保对painter的修改不会影响其他绘制
     painter->save();
 
+    // 当前项状态是否包含了被选中这个标志
     if (option.state & QStyle::State_Selected) {
+        // option.rect是当前项可以绘制的全部矩形区域
+        // option.palette.highlight() 从系统调色板中获取高亮颜色，使应用颜色与系统主题一致
         painter->fillRect(option.rect, option.palette.highlight());
     } else {
         painter->fillRect(option.rect, QColor(240, 248, 255));  // AliceBlue
@@ -29,6 +33,8 @@ void RoomDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
     painter->setPen(QColor(255, 0, 0));
 
     int padding = 5;
+
+    // 这个矩形用于文本绘制，让不同项文本不会紧贴在一起
     QRect textRect = option.rect.adjusted(padding, 0, -padding, 0);
 
     painter->drawText(textRect, Qt::AlignLeft | Qt::AlignVCenter, room.name);
@@ -36,6 +42,7 @@ void RoomDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
     painter->restore();
 }
 
+// 这个函数告诉List每个项的大小
 QSize RoomDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const {
     return QSize(100, 40);
 }
